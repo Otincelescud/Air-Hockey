4b4dd51d888dcb19fcbd987cf09dc6bf5b58eb69
Otincelescu Darius Marian <otincelescud@gmail.com>
2024-04-16 13:57:45 -0700

    Limit frame rate of game window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include "game.h"
 
 Game* game = nullptr;
+const int FPS = 60;
+const int FRAME_DELAY = 1000 / FPS;
+
+Uint32 frame_start;
+int frame_time;
 
 #ifdef _WIN32
 #include <windows.h>
@@ -10,9 +15,15 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     game->init("Air Hockey", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 600, false);
 
     while (game->running()) {
+
+        frame_start = SDL_GetTicks();
+
         game->handle_events();
         game->update();
         game->render();
+
+        frame_time = SDL_GetTicks() - frame_start;
+        if (frame_time < FRAME_DELAY) SDL_Delay(FRAME_DELAY - frame_time);
     }
 
     game->clean();
